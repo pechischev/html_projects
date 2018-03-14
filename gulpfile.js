e66acb04d8c9c8412f1f4cf7719b26e1fcb1979d
Vladi1996mir <https://github.com/pechischev>
2018-03-15 01:39:26 +0300

    реализовал поддержку тестов с использованием модулей

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const tsify = require("tsify");
 const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
 const tslint = require("gulp-tslint");
+const mocha = require('gulp-mocha');
 
 const sassFile = 'css/styles.scss';
 const mainPath = "./src/main.tsx";
@@ -36,7 +37,7 @@ const onError = (err) => {
     this.emit('end');
 };
 
-gulp.task("build_ts", function () {
+gulp.task("build_ts", () => {
 	return browserify({ debug: true })
 		.transform(babelify.configure({ presets : ["es2015"] }))
 		.require(mainPath, { entry: true })
@@ -69,7 +70,15 @@ gulp.task('build_js', (cb) => {
     gulpSequence('tslint', 'build_ts', cb);
 });
 
-gulp.task('build', ['compile_css', 'build_js']);
+gulp.task('test', () => {
+	return gulp.src('test/*.spec.ts')
+		.pipe(mocha({
+			reporter: 'progress',
+			require: ['ts-node/register']
+		}));
+});
+
+gulp.task('build', ['compile_css', 'build_js', 'test']);
 
 gulp.task('watch', () => {
     gulp.watch(sassFile, gulpSequence('compile_css'));
